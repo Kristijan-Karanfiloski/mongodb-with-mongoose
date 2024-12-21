29708ac1810d5833693dd85d34cf2e96faff2bca
Kristijan Karanfiloski <kikaccc29@gmail.com>
2024-12-21 16:30:15 +0100

    Error handling with Express

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const morgan = require("morgan");
 
 const app = express();
 
+const AppError = require("./utils/appError");
+const globalErrorHandler = require("./controllers/errorControllers");
 const tourRouter = require("./routes/tourRoutes");
 const userRouter = require("./routes/userRoutes");
 
@@ -12,24 +14,23 @@ const userRouter = require("./routes/userRoutes");
 ///////////////////// 1) MIDDLEWARES ////////////////////
 
 //// MIDDLEWARE in the middle of the request and response
-console.log(process.env.NODE_ENV);
 
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 app.use(express.json());
-
 //SERVING STATIC FILES FROM A SERVER //////////////////
 app.use(express.static(`${__dirname}/public`));
-
-app.use((req, res, next) => {
-  console.log("Hello from the middleware ");
-  next();
-});
 
 //////////////// MOUNTING OUR ROUTES //////////////////
 
 app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
+
+app.all("*", (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+app.use(globalErrorHandler);
 
 module.exports = app;
---
+++ controllers/errorControllers.js
@@ -0,0 +1,65 @@
+const AppError = require("../utils/appError");
+
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}.`;
+  return new AppError(message, 400);
+};
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+    error: err,
+    stack: err.stack,
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  // Operational, trusted error: send message to client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    // Programming or other unknown error: don't leak error details
+    // 1) Log error
+    console.error("ERROR 💥", err);
+
+    // 2) Send generic message
+    res.status(500).json({
+      status: "error",
+      message: "Something went very wrong!",
+    });
+  }
+};
+
+module.exports = (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500; // 500 means internal server error
+  err.status = err.status || "error";
+
+  if (process.env.NODE_ENV === "development") {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === "production") {
+    let error = { ...err };
+    if (error.name === "Casterroror") {
+      error = handleCastErrorDB(err);
+    }
+    if (error.name === 11000) {
+      const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
+
+      const message = `Duplicate field value: ${value}. Please use another value!`;
+      error = new AppError(message, 400);
+    }
+    if (error.name === "ValidationError") {
+      const errors = Object.values(err.errors).map((el) => el.message);
+      const message = `Invalid input data. ${errors.join(". ")}`;
+      error = new AppError(message, 400);
+    }
+    sendErrorProd(error, res);
+  }
+
+  // res.status(err.statusCode).json({
+  //   status: err.status,
+  //   message: err.message,
+  // });
+};
